54fe8c631cfc1d9f063c5adc582fe27a5da500da
rick23p <peymannaderi3@gmail.com>
2021-07-25 12:16:06 +0430

    fix socket issues.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { ChatGateway } from './chat.gateway';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [],
--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,14 +1,35 @@
+import { Logger } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(80)
-export class ChatGateway {
-  @WebSocketServer()
-  server;
+@WebSocketGateway()
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+    client.emit('message', 'salam')
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
 
   @SubscribeMessage('message')
   handleMessage(@MessageBody() message: string): void {
